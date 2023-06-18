7e109267a9a425021aeed93f6d6066dbc0e3a6f5
blueknarr <knarrblue@gmail.com>
2023-06-18 12:00:45 +0900

    080. WebView 기본 설정

--- lib/main_screen.dart
+++ lib/main_screen.dart
@@ -1,19 +1,44 @@
+import 'dart:io';
+
 import 'package:flutter/material.dart';
+import 'package:webview_flutter/webview_flutter.dart';
 
 class MainScreen extends StatefulWidget {
   const MainScreen({Key? key}) : super(key: key);
 
   @override
-  State<MainScreen> createState() => _MainSceenState();
+  State<MainScreen> createState() => _MainScreenState();
 }
 
-class _MainSceenState extends State<MainScreen> {
+class _MainScreenState extends State<MainScreen> {
+  final controller = WebViewController()
+    ..setJavaScriptMode(JavaScriptMode.unrestricted)
+    ..setBackgroundColor(const Color(0x00000000))
+    ..setNavigationDelegate(
+      NavigationDelegate(
+        onProgress: (int progress) {
+          // Update loading bar.
+        },
+        onPageStarted: (String url) {},
+        onPageFinished: (String url) {},
+        onWebResourceError: (WebResourceError error) {},
+        onNavigationRequest: (NavigationRequest request) {
+          if (request.url.startsWith('https://www.youtube.com/')) {
+            return NavigationDecision.prevent;
+          }
+          return NavigationDecision.navigate;
+        },
+      ),
+    )
+    ..loadRequest(Uri.parse('https://flutter.dev'));
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: const Text('나만의 웹브라우저'),
       ),
+      body: WebViewWidget(controller: controller),
     );
   }
 }
